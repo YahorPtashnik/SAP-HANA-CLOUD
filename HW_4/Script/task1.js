e013fa319459df1d6fb3ea9a8aa51932125ff40c
Yahor Ptashnik <yahor.ptashnik@LeverX.local>
2019-01-05 15:08:05 +0300

    HW_4 Task 1 done

---
+++ HW_4/Script/task1.js
@@ -0,0 +1,18 @@
+var newEl = document.createElement("div");
+var username = prompt("Insert your username", "Username");
+var i = 0;
+if (/[0-9]/.test(username)) {
+    for(i; i<username.length; i++) {
+        if(i%2 == 0) {
+            console.log(username[i].toUpperCase());
+            newEl.innerHTML += username[i].toUpperCase();
+            if(username[i+1]) newEl.innerHTML += username[i+1].toLowerCase();
+        }
+    }
+}
+else {
+newEl.innerHTML = username;
+}
+
+var cont = document.getElementById("container");
+cont.appendChild(newEl);
